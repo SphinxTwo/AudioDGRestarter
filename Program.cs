028da36fc6b86c60bf9a9e14584fcaf5f69f2c12
SphinxTwo <coddude21@hotmail.com>
2018-10-01 15:58:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace AudioDGRestarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("cmd", "/C taskkill /f /im audiodg.exe && audiodg.exe");
+        }
+    }
+}
